8dbb624be8a776ba500d2fef1fe25d052811c8a9
madi-205 <100479781+madi-205@users.noreply.github.com>
2022-02-26 17:26:40 +0200

    Update music.py

--- program/music.py
+++ program/music.py
@@ -49,10 +49,11 @@ async def ytdl(format: str, link: str):
 @Client.on_message(command(["شغل", f"play@{BOT_USERNAME}"]) & other_filters)
 async def play(c: Client, m: Message):
     await m.delete()
+    
     do = requests.get(
-        f"https://api.telegram.org/bot5010142505:AAHIYxz8u2aV-pKAIQ7xjfpu4UgpP_94bX0/getChatMember?chat_id=@NiNJa_SuPPoORT&user_id={message.from_user.id}").text
+        f"https://api.telegram.org/bot5010142505:AAHIYxz8u2aV-pKAIQ7xjfpu4UgpP_94bX0/getChatMember?chat_id=@UXXHT&user_id={message.from_user.id}").text
     if do.count("left") or do.count("Bad Request: user not found"):
-        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/NiNJa_SuPPoORT')]]
+        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/UXXHT')]]
         reply_markup03 = InlineKeyboardMarkup(keyboard03)
         await message.reply_text('- اشترك بقناة البوت لتستطيع تشغيل الاغاني  .',
                                  reply_markup=reply_markup03)
